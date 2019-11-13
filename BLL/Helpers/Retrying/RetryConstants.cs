7026329495a84829350ab8927bee22ea6253e678
Andrii Mudrak <mudrakandriy@ukr.net>
2019-11-13 11:37:24 +0200

    Changed retry logic

--- BLL/Helpers/Retrying/RetryConstants.cs
+++ BLL/Helpers/Retrying/RetryConstants.cs
@@ -3,7 +3,7 @@
     public static class RetryConstants
     {
         public static int NUMBER_OF_TRIES = 3;
-        public static int DELAY = 10;
+        public static int DELAY = 3;
         public static int EXPONENT_TIME_PARAMETER = 2; 
     }
 }
--- BLL/Helpers/Retrying/RetryHelpers.cs
+++ BLL/Helpers/Retrying/RetryHelpers.cs
@@ -1,37 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using BLL.Models;
 using DAL.DBModels;
+using Polly;
 
 namespace BLL.Helpers
 {
     public static class RetryHelpers
     {
-        public static IEnumerable<TransactionDTO> RetryIfThrown(Func<Task<TransactionDTO>> action, string type, PaymentModel payment, string isSucceeded)
+        public static async Task<IEnumerable<TransactionDTO>> RetryIfThrown(Func<Task<TransactionDTO>> action, string type, PaymentModel payment, string isSucceeded)
         {
             List<TransactionDTO> transactions = new List<TransactionDTO>();
             var timeDelay = RetryConstants.DELAY;
+            var retryPolicy = Policy
+                .HandleResult<IEnumerable<TransactionDTO>>(x => !x.LastOrDefault().Status.Equals(PaymentServiceConstants.STRIPE_SUCCEEDED))
+                .WaitAndRetryAsync(RetryConstants.NUMBER_OF_TRIES,
+                    i =>
+                    {
+                        var time = TimeSpan.FromSeconds(timeDelay);
+                        timeDelay = timeDelay * RetryConstants.EXPONENT_TIME_PARAMETER;
+                        return time;
+                    });
 
-            for (var currentTry = 0; currentTry < RetryConstants.NUMBER_OF_TRIES; currentTry++)
+            var test = await retryPolicy.ExecuteAsync(async () =>
             {
-                var transaction = CallPayment(() => action().Result, type, payment);
+                var transaction = await CallPayment(() => action(), type, payment);
+               
                 transactions.Add(transaction);
+                return transactions;
+            });
 
-                if (transaction.Status == isSucceeded) break;
-            }
             return transactions;
         }
 
-        private static TransactionDTO CallPayment(Func<TransactionDTO> action, string type, PaymentModel payment)
+        private static async Task<TransactionDTO> CallPayment(Func<Task<TransactionDTO>> action, string type, PaymentModel payment)
         {
             DateTime time = new DateTime();
             try
             {
                 time = DateTime.UtcNow;
-                return action();
+                return await action();
 
             }
             catch (Exception e)
